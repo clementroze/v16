f72598a0b8da48569ff07e28e48c1ae223183908
Clément Rozé <1369034-Bookie0@users.noreply.replit.com>
2025-07-06 15:56:23 +0000

    Restored to '516ede4ae89f79db91217291636ad4a16f7fb9c6'
    Replit-Restored-To: 516ede4ae89f79db91217291636ad4a16f7fb9c6

--- src/components/CraftPanel.tsx
+++ src/components/CraftPanel.tsx
@@ -1,30 +1,39 @@
+
 import craftData from "../data/craft.json";
 
 export default function CraftPanel() {
+  // Define the layout pattern you specified
+  const layoutPattern = [4, 2, 4, 2, 2, 4, 2, 4, 2, 4, 4, 2, 2, 2, 2, 1];
+  
   return (
     <div className="craft-panel">
       <div className="inner">
         <div className="craft-grid">
-          {craftData.map((item, i) => (
-            <div
-              key={i}
-              className="craft-item"
-              style={{
-                gridColumn: `span ${item.span}`,
-              }}
-            >
-              {item.images.map((img, j) => (
-                <img
-                  key={j}
-                  src={img.src}
-                  alt={img.alt}
-                  style={{
-                    height: `${img.height}px`,
-                  }}
-                />
-              ))}
-            </div>
-          ))}
+          {craftData.map((item, i) => {
+            // Use the layout pattern, cycling through if we have more items than pattern entries
+            const span = layoutPattern[i % layoutPattern.length];
+            
+            return (
+              <div
+                key={i}
+                className="craft-item"
+                style={{
+                  gridColumn: `span ${span}`,
+                }}
+              >
+                {item.images.map((img, j) => (
+                  <img
+                    key={j}
+                    src={img.src}
+                    alt={img.alt}
+                    style={{
+                      height: `${img.height}px`,
+                    }}
+                  />
+                ))}
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
