c3f8925cc06be8ba302bf936d10eeb1dfec024ff
Clément <clementproze@gmail.com>
2025-07-27 14:57:36 +0000

    gud

--- src/Home.tsx
+++ src/Home.tsx
@@ -3,7 +3,6 @@ import Hero from "./components/Hero";
 import Page from "./components/Page";
 import WorkPanel from "./components/WorkPanel";
 import CraftPanel from "./components/CraftPanel";
-import LogoTooltip from "./components/LogoTooltip";
 
 export default function Home() {
 	const [activeTab, setActiveTab] = useState<"work" | "craft">(() => {
@@ -68,78 +67,57 @@ export default function Home() {
 				subheading="is a designer and developer at Cornell who cares about making the web accessible, beautiful, and thoughtful."
 			/>
 
-			<div className="currently-wrapper">
-				<div className="inner">
-					<div className="currently">
-						<div className="row">
-							<p>Currently at</p>
+<div className="currently-wrapper">
+<div className="inner">
+			<div className="currently">
+				<div className="row">
+				<p>Currently at</p>
 
-							<div className="punc">
-								<LogoTooltip
-									logos={[
-										{
-											src: "/home/logos/cornell.png",
-											alt: "Cornell University",
-											tooltip: "Cornell University"
-										}
-									]}
-								/>
-								<p>,</p>
-							</div>
-						</div>
+<div className="punc">
 
-						<div className="row">
-							<p>previously at</p>
-
-							<div className="punc">
-								<LogoTooltip
-									logos={[
-										{
-											src: "/home/logos/frog.png",
-											alt: "Frog Design",
-											tooltip: "Frog Design"
-										},
-										{
-											src: "/home/logos/microsoft.png",
-											alt: "Microsoft",
-											tooltip: "Microsoft"
-										},
-										{
-											src: "/home/logos/replit.png",
-											alt: "Replit",
-											tooltip: "Replit"
-										}
-									]}
-								/>
-
-								<p>,</p>
-							</div>
-						</div>
-
-						<div className="row">
-							<p>and leading teams at</p>
-
-							<div className="punc">
-								<LogoTooltip
-									logos={[
-										{
-											src: "/home/logos/dcc.png",
-											alt: "Design & Tech Collective",
-											tooltip: "Design & Tech Collective"
-										},
-										{
-											src: "/home/logos/dti.png",
-											alt: "Digital Technology & Innovation",
-											tooltip: "Digital Technology & Innovation"
-										}
-									]}
-								/>
-
-								<p>.</p>
-							</div>
-						</div>
-					</div>
+				<div className="logos">
+					<img src="/home/logos/cornell.png" alt=""/>
 				</div>
+				<p>,</p>
+
+				</div>
+</div>
+
+
+
+<div className="row">
+				<p>previously at</p>
+
+<div className="punc">
+				<div className="logos">
+					<img src="/home/logos/frog.png" alt=""/>
+
+					<img src="/home/logos/microsoft.png" alt=""/>
+
+					<img src="/home/logos/replit.png" alt=""/>
+				</div>
+
+				<p>,</p>
+				</div>
+				</div>
+
+				<div className="row">
+
+				<p>and leading teams at</p>
+
+<div className="punc">
+
+				<div className="logos">
+					<img src="/home/logos/dcc.png" alt=""/>
+
+					<img src="/home/logos/dti.png" alt=""/>
+				</div>
+
+				<p>.</p>
+				</div>
+			</div>
+			</div>
+			</div>
 			</div>
 
 			<div className="tabs-container">
